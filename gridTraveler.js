75cb6ac3d76525d9fb83cf72d2327eb39883d0d0
A. Legayada <tonilegayada@gmail.com>
2024-10-10 01:29:11 +0800

    added memoization

--- gridTraveler.js
+++ gridTraveler.js
@@ -1,1 +1,33 @@
-node_modules
+const gridTraveler = (m, n) => {
+  if (n == 1 && m == 1) {
+    return 1
+  }
+  if (n == 0 || m == 0) {
+    return 0
+  }
+
+  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+}
+
+console.log(gridTraveler(1, 1))
+console.log(gridTraveler(2, 3))
+
+const betterGrid = (m, n, memo = {}) => {
+  const key = m + ',' + n
+  if (key in memo) {
+    return memo[key]
+  }
+  if (n == 1 && m == 1) {
+    return 1
+  }
+  if (n == 0 || m == 0) {
+    return 0
+  }
+
+  memo[key] = betterGrid(m - 1, n, memo) + betterGrid(m, n - 1, memo)
+
+  return memo[key]
+}
+
+console.log(betterGrid(18, 20))
+console.log(gridTraveler(18, 20))
