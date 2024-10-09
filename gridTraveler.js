90d2a245f56760a80d3e7bd27c449088360c38b3
A. Legayada <tonilegayada@gmail.com>
2024-10-10 00:32:59 +0800

    removed unecessary files

--- gridTraveler.js
+++ gridTraveler.js
@@ -1,13 +1,1 @@
-const gridTraveler = (m, n) => {
-  if (n == 1 && m == 1) {
-    return 1
-  }
-  if (n == 0 || m == 0) {
-    return 0
-  }
-
-  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
-}
-
-console.log(gridTraveler(1, 1))
-console.log(gridTraveler(2, 3))
+node_modules
